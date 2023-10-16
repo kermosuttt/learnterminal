e9e0866550c9fcb6df6dc0776d99d0d1ee84af70
kermosuttt <kermo.sutt@tptlive.ee>
2023-10-16 04:02:12 +0300

    6 different boxes

--- main.js
+++ main.js
@@ -5,10 +5,17 @@ function write(text) {
     process.stdout.write(text);
 }
 
-console.log(chalk.bgGreen.magenta('Hello world'));
-console.log(chalk.underline('Hello world'));
-console.log(chalk.italic('Hello world'));
-console.log(chalk.strikethrough('Hello world'));
-console.log(chalk.inverse('Hello world'));
+// Näide
+//console.log(boxen(chalk.bgGreen.magenta('Hello world'), {borderStyle:'round'}));
 
-console.log(boxen(chalk.bgGreen.magenta('Hello world'), {borderStyle:'round'}));
+console.log(boxen(chalk.bgWhite.rgb(0,0,255).underline('TERE!'), {padding: 6.9, margin: 6.9, borderStyle: 'classic'}));
+
+console.log(boxen(chalk.bgBlue.magentaBright('KERMO'), {padding: 6.9, margin: 9, borderStyle: 'bold', width: 12, borderColor: 'red'}));
+
+console.log(boxen(chalk.overline.bgCyanBright.bold('sUtT'), {borderStyle:'singleDouble', fullscreen: (2, 2), borderColor: 'cyan'}));
+
+console.log(boxen(chalk.hex('#DEADED').bgBlack.italic('on'), {padding: 15, margin: 7.69, borderStyle:'arrow', title: 'jap', borderColor: 'green'}));
+
+console.log(boxen(chalk.bgYellow.strikethrough('Väga'), {borderStyle:'doubleSingle', padding: 4.20, margin: 9.2, title: 'Vääga', titleAlignment: 'center', borderColor: '#ff2022'}));
+
+console.log(boxen(chalk.bgAnsi256(200)('PRO'), {borderStyle:'round', height: 16, width: 7, borderColor: 'yellow'}));
