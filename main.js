15c264e370f713b3ba0db4e1cbc8602e49cd3d3b
Kermo Sutt <kermo.sutt@tptlive.ee>
2023-10-16 10:59:31 +0300

    Add inquirer questions

--- main.js
+++ main.js
@@ -1,45 +1,32 @@
 import chalk from 'chalk';
 import boxen from 'boxen';
 import readline from 'readline';
+import inquirer from 'inquirer';
 
 function write(text) {
-    process.stdout.write(text);
-}
-console.clear();
-readline.emitKeypressEvents(process.stdin);
-
-if(process.stdin.isTTY){
-    process.stdin.setRawMode(true);
+process.stdout.write(text);
 }
 
 
-write('@');
-
-process.stdin.on('keypress', (chunk, key) => {
-    if(key && key.name === 'c' && key.ctrl === true){
-        process.exit();
-    }
-    if(key && key.name === 'd'){
-       write('\x1B[1D');
-       write(' @'); 
-    }
-    if(key && key.name === 's'){
-        write('\x1B[1D');
-        write(' ');
-        write('\x1B[1D');
-        write('\x1B[1B');
-        write('@');
-    }
-    if(key && key.name === 'a'){
-        write('\x1B[2D');
-        write('@ '); 
-        write('\x1B[1D');
-    }
-    if(key && key.name === 'w'){
-        write('\x1B[1D');
-        write(' ');
-        write('\x1B[1D');
-        write('\x1B[1A');
-        write('@');
-    }
+inquirer
+.prompt([
+{type:'input', name:'name', message: 'What is your name?'},
+{type:'list', name:'gender', message:'What is your gender?', choices: ['male', 'female', 'bigender', 'apache helicopter', 'other']},
+{type:'number', name:'number', message: 'What is your number?'},
+{type:'confirm', name:'confirm', message: 'Are you an idiot?'},
+{type:'password', name:'password', message: 'Enter a password'},
+{type:'checkbox', name:'items', message: 'Select items to use in a zombie apocolypse', choices: ['knife', 'machete', 'pistol', 'machine gun', 'fork']},
+{type:'rawlist', name:'size', message: 'What clothing size do you need?', choices: ['s', 'm', 'l', 'xl', 'xxl']},
+{type:'expand', name:'expand', message: 'Conflict on file.js', choices: ['delete','overwrite','ignore', 'abort']},
+{type:'editor', name:'editor', message: 'Select your preferred editor?'},
+])
+.then((answers) => {
+console.log(answers);
+})
+.catch((error) => {
+if (error.isTtyError){
+// Prompt couldn't be rendered in the current enviroment
+} else {
+//Something else went wrong
+}
 });
