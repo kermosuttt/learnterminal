8263d06c0334bc75a81709293e67daea51efd983
kermo.sutt@tptlive.ee <kermo.sutt@tptlive.ee>
2023-10-18 05:14:51 +0300

    add yargs example

--- main.js
+++ main.js
@@ -1,32 +1,4 @@
-import chalk from 'chalk';
-import boxen from 'boxen';
-import readline from 'readline';
-import inquirer from 'inquirer';
-
-function write(text) {
-process.stdout.write(text);
-}
-
-
-inquirer
-.prompt([
-{type:'input', name:'name', message: 'What is your name?'},
-{type:'list', name:'gender', message:'What is your gender?', choices: ['male', 'female', 'bigender', 'apache helicopter', 'other']},
-{type:'number', name:'number', message: 'What is your number?'},
-{type:'confirm', name:'confirm', message: 'Are you an idiot?'},
-{type:'password', name:'password', message: 'Enter a password'},
-{type:'checkbox', name:'items', message: 'Select items to use in a zombie apocolypse', choices: ['knife', 'machete', 'pistol', 'machine gun', 'fork']},
-{type:'rawlist', name:'size', message: 'What clothing size do you need?', choices: ['s', 'm', 'l', 'xl', 'xxl']},
-{type:'expand', name:'expand', message: 'Conflict on file.js', choices: ['delete','overwrite','ignore', 'abort']},
-{type:'editor', name:'editor', message: 'Select your preferred editor?'},
-])
-.then((answers) => {
-console.log(answers);
-})
-.catch((error) => {
-if (error.isTtyError){
-// Prompt couldn't be rendered in the current enviroment
-} else {
-//Something else went wrong
-}
-});
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers'
+const argv = yargs(hideBin(process.argv)).argv
+console.log(argv.value1 + ' ' + argv.sign + ' ' + argv.value2 + ' = ' );
