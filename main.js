92bd43c1d2713e55eb464c836abb30345c499b15
kermo.sutt@tptlive.ee <kermo.sutt@tptlive.ee>
2023-11-13 19:58:44 +0200

    add yargs calculator example

--- main.js
+++ main.js
@@ -1,4 +1,48 @@
 import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers'
-const argv = yargs(hideBin(process.argv)).argv
-console.log(argv.value1 + ' ' + argv.sign + ' ' + argv.value2 + ' = ' );
+import { hideBin } from 'yargs/helpers';
+
+const argv = yargs(hideBin(process.argv))
+  .option('value1', {
+    alias: 'v1',
+    describe: 'First operand',
+    demandOption: true,
+    type: 'number',
+  })
+  .option('value2', {
+    alias: 'v2',
+    describe: 'Second operand',
+    demandOption: true,
+    type: 'number',
+  })
+  .option('sign', {
+    alias: 's',
+    describe: 'Math operation: +, -, *, div',
+    demandOption: true,
+    choices: ['+', '-', '*', 'div'],
+  })
+  .help()
+  .argv;
+
+const { value1, value2, sign } = argv;
+
+let result;
+
+switch (sign) {
+  case '+':
+    result = value1 + value2;
+    break;
+  case '-':
+    result = value1 - value2;
+    break;
+  case '*':
+    result = value1 * value2;
+    break;
+  case 'div':
+    result = value1 / value2;
+    break;
+  default:
+    console.error('Invalid sign');
+    process.exit(1);
+}
+
+console.log(`${value1} ${sign} ${value2} = ${result}`);
